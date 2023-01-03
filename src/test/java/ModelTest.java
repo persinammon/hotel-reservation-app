e3e18b311cee120b641b5d7a7063275d3e03f595
Monica Kumaran <persinammon@users.noreply.github.com>
2023-01-02 23:44:06 -0800

    Tests for string reprs have been finished

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ModelTest {
@@ -17,6 +20,18 @@ public class ModelTest {
     public void createCustomerWorks() {
         Customer customer1 = new Customer("Jamey", "Dogman", "jamey.dogman@hotmail.com");
         assertNotNull(customer1, "Customer was not created"); //second parameter is an optional failure message
+    }
+
+    @Test
+    @DisplayName("Room creation with valid room number works")
+    public void createRoomWorks() {
+
+    }
+
+    @Test
+    @DisplayName("Reservation creation with valid parameters works")
+    public void createReservationWorks() {
+
     }
 
     @ParameterizedTest
@@ -35,40 +50,88 @@ public class ModelTest {
         //fill in
     }
 
+    @Test
     @DisplayName("String representation of Customer has all information")
     public void stringCustomer_containsAllValues() {
+        String firstName = "Jorge";
+        String lastName = "Drooey";
+        String email = "jaredooey@gmail.com";
+        Customer cust = new Customer(firstName, lastName, email);
+        String custString = cust.toString();
+        assertAll(() -> assertTrue(custString.contains(firstName)),
+                () -> assertTrue(custString.contains(lastName)),
+                () -> assertTrue(custString.contains(email)));
     }
-    @DisplayName("String representation of Customer has all information")
+
+    @Test
+    @DisplayName("String representation of Room has all information")
     public void stringRoom_containsAllValues() {
-
+        String roomNumber = "434";
+        Double price = 5.0;
+        RoomType enumeration = RoomType.SINGLE;
+        Room room = new Room(roomNumber, price, enumeration);
+        String roomString = room.toString();
+        assertAll(() -> assertTrue(roomString.contains(roomNumber)),
+                () -> assertTrue(roomString.contains(price.toString())),
+                () -> assertTrue(roomString.contains(enumeration.toString())));
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("String representation of Free Room has all information")
     public void stringFreeRoom_containsAllValues() {
-
+        String roomNumber = "434";
+        Double price = 5.0;
+        RoomType enumeration = RoomType.SINGLE;
+        Room room = new FreeRoom(roomNumber, enumeration);
+        String roomString = room.toString();
+        assertAll(()->assertTrue(roomString.contains("free")||roomString.contains("Free")),
+                ()->assertTrue(roomString.contains(roomNumber)),
+                ()->assertTrue(roomString.contains(((Double) 0.0).toString())),
+                ()->assertTrue(roomString.contains(enumeration.toString())));
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("String representation of Reservation has all information")
     public void stringReservation_containsAllValues() {
-
+        Customer cust = new Customer("Jupiter", "Dionysus", "jdude@gmail.com");
+        Room room = new Room("500", 2.0, RoomType.DOUBLE);
+        String custString = cust.toString();
+        String roomString = room.toString();
+        Date checkInDate = new Date();
+        try {
+            Thread.sleep(2000); //2 seconds, Date's smallest granularity is by the second
+        } catch (Exception e) {}
+        finally {
+            Date checkOutDate = new Date();
+            Reservation reserve = new Reservation(cust, room, checkInDate, checkOutDate);
+            String reserveString = reserve.toString();
+            assertAll(() -> assertTrue(reserveString.contains(custString)),
+                    () -> assertTrue(reserveString.contains(roomString)),
+                    () -> assertTrue(reserveString.contains(checkInDate.toString())),
+                    () -> assertTrue(reserveString.contains(checkOutDate.toString())));
+        }
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("")
     public void equalsCustomer_returnsTrue() {
 
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("")
     public void notEqualsCustomer_returnsFalse() {
 
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("")
     public void equalsRoom_returnsTrue() {
 
     }
 
-    @DisplayName("String representation of Customer has all information")
+    @Test
+    @DisplayName("")
     public void notEqualsRoom_returnsFalse() {
 
     }
